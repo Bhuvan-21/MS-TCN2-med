9a61a6e78ba636813d510ffc9f8250bf8b5ea0eb
AgenoDrei <s.mueller1995@gmail.com>
2024-09-01 17:02:11 +0200

    Fix video length difference caused by fps rounding error

--- scripts/anonymize_videos.py
+++ scripts/anonymize_videos.py
@@ -4,6 +4,15 @@ import cv2
 from tqdm.auto import tqdm
 
 
+def get_video_duration(video_path):
+    cap = cv2.VideoCapture(video_path)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    duration = frame_count / fps
+    cap.release()
+    return duration
+
+
 @click.command()
 @click.option('--input_dir', '-i', required=True, help='Input directory containing videos')
 @click.option('--output_dir', '-o', required=True, help='Output directory to save anonymized videos')
@@ -17,7 +26,8 @@ def anonymize_videos(input_dir, output_dir):
         width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
         height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
         box_height = height // 15
-        fps = int(cap.get(cv2.CAP_PROP_FPS))
+        box_indent = width // 8.5
+        fps = cap.get(cv2.CAP_PROP_FPS)
         
         fourcc = cv2.VideoWriter_fourcc(*'avc1')
         out = cv2.VideoWriter(os.path.join(output_dir, video), fourcc, fps, (width, height))
@@ -28,14 +38,17 @@ def anonymize_videos(input_dir, output_dir):
                 break
 
             # Apply Gaussian blur to the first 100 rows
-            frame[:box_height, 220:] = cv2.boxFilter(frame[:box_height, 220:], -1, (15, 15)) #cv2.blur(frame[:box_height, :], (20, 20), 0)
+            frame[:box_height, box_indent:] = cv2.boxFilter(frame[:box_height, box_indent:], -1, (15, 15)) #cv2.blur(frame[:box_height, :], (20, 20), 0)
             out.write(frame) # Write the frame to the output video
         
         cap.release()
         out.release()
         # break # stop after processing one video
+        duration_src, duration_des = get_video_duration(video_path), get_video_duration(os.path.join(output_dir, video))
+        print(video, duration_src == duration_des, duration_src, duration_des)
+
     
 
 if __name__ == '__main__':
-    anonymize_videos()
+    anonymize_videos(None, None)
     
